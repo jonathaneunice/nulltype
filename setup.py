fb5bd78e8caff8451331cbf50d457ed94e55c484
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-08-04 12:04:03 -0400

    2.1.5 fixes packaging nit

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def linelist(text):
 
 setup(
     name='nulltype',
-    version='2.1.4',
+    version='2.1.5',
     author='Jonathan Eunice',
     author_email='jonathan.eunice@gmail.com',
     description='Null values and sentinels like, but not, None',
