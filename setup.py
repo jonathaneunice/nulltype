e3e89c7e0146b5219e59bf0f7cfe63be61eb2ecf
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-08-10 11:26:54 -0400

    2.1.6

--- setup.py
+++ setup.py
@@ -1,35 +1,15 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-import sys
+from codecs import open
 
 
-class Tox(TestCommand):
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        errno = tox.cmdline(args=args)
-        sys.exit(errno)
-
-
-def linelist(text):
+def lines(text):
     """
     Returns each non-blank line in text enclosed in a list.
+    See http://pypi.python.org/pypi/textdata for more sophisticated version.
     """
-    return [l.strip() for l in text.strip().splitlines() if l.split()]
+    return [l.strip() for l in text.strip().splitlines() if l.strip()]
 
 
 setup(
@@ -38,17 +18,17 @@ setup(
     author='Jonathan Eunice',
     author_email='jonathan.eunice@gmail.com',
     description='Null values and sentinels like, but not, None',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst', encoding='utf-8').read(),
     url='https://bitbucket.org/jeunice/nulltype',
     license='Apache License 2.0',
     py_modules=['nulltype'],
+	setup_requires=[],
     install_requires=[],
     tests_require=['tox', 'pytest'],
     test_suite="test",
-    cmdclass = {'test': Tox},
     zip_safe=False,
     keywords='null none singleton sentinel',
-    classifiers=linelist("""
+    classifiers=lines("""
         Development Status :: 5 - Production/Stable
         Operating System :: OS Independent
         License :: OSI Approved :: Apache Software License
